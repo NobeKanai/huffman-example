c1509fb3d2a3ad31a347537f99475ac8b8c313b7
NobeKanai <qidada4880@gmail.com>
2020-11-19 13:59:32 +0800

    fix bug

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === 'production' && !process.env.IS_NETLIFY === 'yes'
+  publicPath: process.env.NODE_ENV === 'production' && !process.env.IS_NETLIFY
     ? '/huffman/'
     : '/'
 }
